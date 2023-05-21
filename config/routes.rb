dd2e82b34cdf55c9c9821dab03d0bf6db3db02eb
Parker Hodge <iparkerhodge@gmail.com>
2023-05-21 13:48:41 -0500

    big commit - add users, auth, and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
-  root "home#index"
+  # Static Pages
+  root 'home#index'
   get '/how-it-works', to: 'about#index'
   get '/manage', to: 'manage#index'
+
+  # Sign Up
+  post 'sign_up', to: 'users#create'
+  get 'sign_up', to: 'users#new'
+
+  # Confirmations
+  resources :confirmations, only: %i[create edit new], param: :confirmation_token
+
+  # Sessions
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'sessions#new'
+
+  # Active Sessions
+  resources :active_sessions, only: [:destroy] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+
+  # Passwords
+  resources :passwords, only: %i[create edit new update], param: :password_reset_token
+
+  # Account
+  put 'account', to: 'users#update'
+  get 'account', to: 'users#edit'
+  delete 'account', to: 'users#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_204757) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_21_184052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.index ["user_id"], name: "index_active_sessions_on_user_id"
+  end
 
   create_table "cards", force: :cascade do |t|
     t.string "front"
@@ -21,4 +30,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_204757) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
+    t.string "unconfirmed_email"
+    t.string "remember_token", null: false
+    t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
+  end
+
+  add_foreign_key "active_sessions", "users", on_delete: :cascade
 end
